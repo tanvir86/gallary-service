e53d6380bd28a750d45ddccac6305aba0487bfea
Tanvir Ahmmed <ahmed.tanvir.010@gmail.com>
2019-04-01 09:45:23 +0600

    initiate

---
+++ src/main/java/com/practice/microservice/galleryservice/entities/Gallery.java
@@ -0,0 +1,27 @@
+package com.practice.microservice.galleryservice.entities;
+
+import java.util.List;
+
+public class Gallery {
+    private int id;
+    private List<Object> images;
+
+    public Gallery() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Object> getImages() {
+        return images;
+    }
+
+    public void setImages(List<Object> images) {
+        this.images = images;
+    }
+}
